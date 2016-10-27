816f44fb4917abc7d691a0ea59fe11c2ff89c5a7
ChlineM <sarah.berastegui@gmail.com>
2016-10-27 09:31:20 +0200

    suppression d'une ligne vide

--- 02_ProjetGitMaven/src/main/java/com/mii/gitmaven/Main.java
+++ 02_ProjetGitMaven/src/main/java/com/mii/gitmaven/Main.java
@@ -3,10 +3,8 @@ package com.mii.gitmaven;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Message obj = new Message();
 		System.out.println(obj.getMessage("jenkins"));
-
 	}
 
 }
